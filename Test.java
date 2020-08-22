fe17b66454e4805ff1b7d22902d88a1ce52070e2
Val1809 <60783205+Val1809@users.noreply.github.com>
2020-08-22 10:02:34 -0500

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,23 @@
+package model;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.awt.Menu;
+
+import model.Test.test;
+class Test {
+ 
+	public @interface test {
+
+	}
+
+	@org.junit.jupiter.api.Test
+	//@Test//
+	void test() {
+@test 
+		public void testMenu() {
+			
+		}
+	}
+
+}
